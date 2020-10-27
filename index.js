d5ce1728154f387092dec57f61a799a5889f12f5
Alex Jabbour <redpod22@gmail.com>
2020-10-27 02:35:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+// this program was written by Alex Jabbour on oct 26th
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
 
 async function handleRequest(request) {
+	const url = new URL(request.url);
 
-	return new Response('Works!', {
+	// check if the pathname is '/links'
+	
+
+	console.log(url.pathname)
+
+	return new Response("works \n", {
 		headers: { 'content-type': 'application/json' },
 	})
 }
